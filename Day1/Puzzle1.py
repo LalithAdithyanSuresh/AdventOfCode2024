894259d32b4233d9e9295ad3c1cd41f0f6888bdd
Lalith Adithyan Suresh <140822743+LalithAdithyanSuresh@users.noreply.github.com>
2024-12-01 22:00:34 +0530

    DAY1

--- Day1/Puzzle1.py
+++ Day1/Puzzle1.py
@@ -3,11 +3,16 @@ path = sys.argv[1]
 with open(path) as f:
     text = f.readlines()
 
+# Getting the 2 lists ready
 l1,l2 = [],[]
 for line in text:
     l1.append(line.split()[0])
     l2.append(line.split()[1])
+
+# Sorting the lists
 l1,l2 = sorted(l1),sorted(l2)
+
+# Calculating differences between pairs
 d = 0
 for i in range(len(l1)):
     d += abs(int(l1[i]) - int(l2[i]))
--- Day1/Puzzle2.py
+++ Day1/Puzzle2.py
@@ -2,10 +2,14 @@ import sys
 path = sys.argv[1]
 with open(path) as f:
     text = f.readlines()
+
+# Getting the 2 lists ready
 l1,l2 = [],[]
 for line in text:
     l1.append(line.split()[0])
     l2.append(line.split()[1])
+
+# Calculating the count * value
 d = 0
 for i in range(len(l1)):
     d += int(l1[i]) * l2.count(l1[i])
